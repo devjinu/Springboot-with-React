1ea364e6ce542cb11373837bec6cbff4ec47d2b6
moonjinu <69113782+moonjinu@users.noreply.github.com>
2022-03-02 14:03:29 +0900

    useState

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from "react";
 
 // 1. return시에 하나의 Dom만 리턴할 수 있다
 // 2. 변수선언은 let 혹은 const로만 해야함
@@ -13,22 +14,30 @@ let a = 10; // 변수
 const b = 20; // 상수
 
 function App() {
-    let c;
-    let d = undefined;
-    console.log(1, c);
 
-    const myStyle = {
-        color: "red",
+    const [num, setNum] = useState(5);
+
+    let sample=[
+        {id:1,name:"홍길동"},
+        {id:2,name:"임꺽정"},
+        {id:3,name:"장보고"},
+        {id:4,name:"이순신"}
+    ]
+    console.log("App 실행됨")
+    const [users, setUsers] = useState(sample); // 레퍼런스 변경되야 동작
+
+    const download= ()=>{
+        // fetch().then().then();
+        // 기존 데이터 세팅
+        setUsers([...sample,{id:num,name:"길동"}]);
+        setNum(num +1);
     }
 
-    let list = [1, 2, 3];
-
+    // 렌더링 시점 = 상태값 변경
     return (
         <div>
-            <div style={myStyle}>안녕{a === 10 ? " a는 10입니다." : " a는 10이 아닙니다."}</div>
-            <h1 className={"box-style"}>헤딩태크{b === 20 && '20입니다'}</h1>
-            <div> {list.map(n => <h1>{n}</h1>)}</div>
-            <hr/>
+        <button onClick={download}>다운로드</button>
+            {users.map(u=><h1>{u.id},{u.name}</h1>)}
         </div>
     );
 }
